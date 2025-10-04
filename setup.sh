c4985c9b003c36225547811fc5e99e2b3fa719a2
ibrahimahtsham <ibrahimahtsham2002@gmail.com>
2025-10-04 14:43:32 +0500

    Remove 'less' from package installation and comment out git config prompts in setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo pacman -Syu --noconfirm --needed vim git fastfetch base-devel openssh less
+sudo pacman -Syu --noconfirm --needed vim git fastfetch base-devel
 
 sudo systemctl enable sshd
 sudo systemctl start sshd
@@ -14,10 +14,3 @@ cd src
 git clone https://github.com/ibrahimahtsham/arch-dwm.git
 cd arch-dwm
 
-# ask username and email to set git config
-read -p "Enter your git username: " git_username
-read -p "Enter your git email: " git_email
-git config --global user.name "$git_username"
-git config --global user.email "$git_email"
-
-git config --global --list
