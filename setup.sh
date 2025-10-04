121f46c02169eb6fe6c7282fe6b8c617511dc8eb
ibrahimahtsham <ibrahimahtsham2002@gmail.com>
2025-10-04 13:23:41 +0500

    Add fastfetch to dependencies in setup script

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # Install deps for building/running dwm
-sudo pacman -Syu --noconfirm --needed base-devel
+sudo pacman -Syu --noconfirm --needed base-devel fastfetch
 
 # Ensure we operate from ~/.local/src/arch-dwm
 cd ~/.local/src/arch-dwm
