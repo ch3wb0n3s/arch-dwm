4509e3d5793bbb03863657c4f45edf4cad416da0
ibrahimahtsham <ibrahimahtsham2002@gmail.com>
2025-10-04 15:34:31 +0500

    Update setup script and add configuration files for dwm

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-sudo pacman -Syu --noconfirm --needed vim git fastfetch base-devel
+sudo pacman -Syu --noconfirm --needed vim git fastfetch base-devel ttf-dejavu
 
 sudo systemctl enable sshd
 sudo systemctl start sshd
-
 
 # make directories and clone repo from which dwm configs will be set
 mkdir .local
@@ -14,3 +13,14 @@ cd src
 git clone https://github.com/ibrahimahtsham/arch-dwm.git
 cd arch-dwm
 
+# clone dwm and other suckless tools
+git clone https://aur.archlinux.org/dwm.git
+cd dwm
+rm -rf .git
+
+# make dwm
+makepkg -Ccfsi --noconfirm
+
+# put exec dwm in .xinitrc
+cd ~
+echo "exec dwm" > .xinitrc
