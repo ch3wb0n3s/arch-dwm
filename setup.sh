3a4acd04eed35303d3d9a6b2c7cb27153f33136a
ibrahimahtsham <ibrahimahtsham2002@gmail.com>
2025-10-04 14:26:52 +0500

    Refactor setup script: streamline package installation and enable SSH service

--- setup.sh
+++ setup.sh
@@ -1,31 +1,6 @@
 #!/usr/bin/env bash
 
-# echo test in green
-# echo -e "\e[32m test \e[0m"
+sudo pacman -Syu --noconfirm --needed vim git fastfetch base-devel openssh
 
-sudo pacman -Syu --noconfirm --needed vim git fastfetch
-
-mkdir -p ~/.local/src && cd ~/.local/src
-git clone https://github.com/ibrahimahtsham/arch-dwm.git
-cd arch-dwm
-
-# Install deps for building/running dwm
-sudo pacman -Syu --noconfirm --needed base-devel
-
-# Ensure we operate from ~/.local/src/arch-dwm
-cd ~/.local/src/arch-dwm
-
-# Vendor dwm (clone into ./dwm and remove its .git), or just build if present
-if [[ ! -d dwm ]]; then
-  git clone --depth=1 https://git.suckless.org/dwm dwm
-  rm -rf dwm/.git
-fi
-
-# Ensure xinit starts dwm (append only once)
-if ! grep -qx 'exec dwm' "$HOME/.xinitrc" 2>/dev/null; then
-  echo "exec dwm" >> "$HOME/.xinitrc"
-fi
-
-cd dwm
-make
-sudo make install
+sudo systemctl enable sshd
+sudo systemctl start sshd
