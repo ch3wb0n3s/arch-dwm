e93258ab44138ad634c51d7174f2cf7c8dceddeb
ibrahimahtsham <ibrahimahtsham2002@gmail.com>
2025-10-04 14:09:12 +0500

    Uncomment installation steps in setup script for clarity

--- setup.sh
+++ setup.sh
@@ -1,31 +1,31 @@
 #!/usr/bin/env bash
 
 # echo test in green
-echo -e "\e[32m test \e[0m"
+# echo -e "\e[32m test \e[0m"
 
-# sudo pacman -Syu vim git fastfetch
+sudo pacman -Syu vim git fastfetch
 
-# mkdir -p ~/.local/src && cd ~/.local/src
-# git clone https://github.com/ibrahimahtsham/arch-dwm.git
-# cd arch-dwm
+mkdir -p ~/.local/src && cd ~/.local/src
+git clone https://github.com/ibrahimahtsham/arch-dwm.git
+cd arch-dwm
 
-# # Install deps for building/running dwm
-# sudo pacman -Syu --noconfirm --needed base-devel
+# Install deps for building/running dwm
+sudo pacman -Syu --noconfirm --needed base-devel
 
-# # Ensure we operate from ~/.local/src/arch-dwm
-# cd ~/.local/src/arch-dwm
+# Ensure we operate from ~/.local/src/arch-dwm
+cd ~/.local/src/arch-dwm
 
-# # Vendor dwm (clone into ./dwm and remove its .git), or just build if present
-# if [[ ! -d dwm ]]; then
-#   git clone --depth=1 https://git.suckless.org/dwm dwm
-#   rm -rf dwm/.git
-# fi
+# Vendor dwm (clone into ./dwm and remove its .git), or just build if present
+if [[ ! -d dwm ]]; then
+  git clone --depth=1 https://git.suckless.org/dwm dwm
+  rm -rf dwm/.git
+fi
 
-# # Ensure xinit starts dwm (append only once)
-# if ! grep -qx 'exec dwm' "$HOME/.xinitrc" 2>/dev/null; then
-#   echo "exec dwm" >> "$HOME/.xinitrc"
-# fi
+# Ensure xinit starts dwm (append only once)
+if ! grep -qx 'exec dwm' "$HOME/.xinitrc" 2>/dev/null; then
+  echo "exec dwm" >> "$HOME/.xinitrc"
+fi
 
-# cd dwm
-# make
-# sudo make install
+cd dwm
+make
+sudo make install
